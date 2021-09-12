eea3beeb271d707e36785d5e2c986797de05c09a
[Nick Cruz] <[nickcruz520@gmail.com]>
2021-09-11 21:35:39 -0400

    Added FAQ partial. added faq collapsible section to About us section

--- js/main.js
+++ js/main.js
@@ -10,3 +10,20 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+// Collapsible (FAQ Section) // 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
