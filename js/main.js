84f646ef660f47c96fa62a920bc61bed994c253e
[Nick Cruz] <[nickcruz520@gmail.com]>
2021-09-11 22:27:16 -0400

    Added clock picture and next semester section in css. Also added next semester countdown timer to home page

--- js/main.js
+++ js/main.js
@@ -27,3 +27,35 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+// Next Semester Countdown // 
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 25, 2022 12:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("countDown").innerHTML = days + " Days " + hours + " Hours "
+  + minutes + " Minutes " + seconds + " Seconds ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countDown").innerHTML = "The Semester Has Started";
+  }
+}, 0);
